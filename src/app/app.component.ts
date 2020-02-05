854380b489afac7d979d66e76ef9299dc77c2d9e
Migal Bothma <migal.bothma@bcx.co.za>
2020-02-05 19:51:22 +0200

    service workers fixed dist build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'eliot-web';
+public ngOnInit():void {if (this.swUpdate.isEnabled) {
+    this.swUpdate.available.subscribe((evt) => {
+        console.log('service worker updated');
+    });
+
+    this.swUpdate.checkForUpdate().then(() => {
+        // noop
+    }).catch((err) => {
+        console.error('error when checking for update', err);
+    });
+}}
+
+
+ constructor(private swUpdate: SwUpdate) {}
 }
